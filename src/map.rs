52be83d1996792acf8d70e37a17fdc4dc8d9d940
Joel Lauener <joellauener@hotmail.com>
2019-11-11 22:04:04 +0100

    Fix map index bug

--- src/map.rs
+++ src/map.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::entity::EntityId;
 use crate::map_data::MapData;
 use crate::position::Position;
@@ -27,36 +29,34 @@ impl Tile {
 }
 
 pub struct Map {
-    tiles: Vec<Tile>,
-    width: usize,
+    tiles: HashMap<(i16,i16), Tile>,
 }
 
 impl Map {
     pub fn new(data: MapData) -> Map {
-        let mut tiles: Vec<Tile> = Vec::new();
+        let mut tiles: HashMap<(i16, i16), Tile> = HashMap::new();
         for iy in 0..data.get_height() {
             for ix in 0..data.get_width() {
                 let tile = Tile::new(ix as i16, iy as i16, data.get_tile(ix, iy));
-                tiles.push(tile);
+                tiles.insert((ix as i16, iy as i16), tile);
             }
         }
 
         Map {
             tiles,
-            width: data.get_width(),
         }
     }
 
     pub fn get(&self, pos: Position) -> Option<&Tile> {
-        self.tiles.get(pos.y as usize * self.width + pos.x as usize)
+        self.tiles.get(&(pos.x, pos.y))
     }
 
     pub fn get_mut(&mut self, pos: Position) -> Option<&mut Tile> {
-        self.tiles.get_mut(pos.y as usize * self.width + pos.x as usize)
+        self.tiles.get_mut(&(pos.x, pos.y))
     }
 
     pub fn for_each<F>(&self, mut action: F) where F: FnMut(&Tile) {
-        for tile in &self.tiles {
+        for (_, tile) in &self.tiles {
             action(tile);
         }
     }
